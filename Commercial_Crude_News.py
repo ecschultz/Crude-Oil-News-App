e1f3033fe3d04f9ca49998bd89db08f4e55cccee
Ethan Schultz <88568303+ecschultz@users.noreply.github.com>
2023-07-12 08:50:30 -0500

    Update Commercial_Crude_News.py
    st.dataframe(df.style.applymap(make_clickable, subset=['Link']))

--- Commercial_Crude_News.py
+++ Commercial_Crude_News.py
@@ -85,6 +85,8 @@ st.image(cop_image, caption='News Updated every ** Minutes')
 ### Add RSS Feed Dataframe to the app
 #st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
 #st.dataframe(df, use_container_width=True)
+
+st.dataframe(df.style.applymap(make_clickable, subset=['Link']))
 st.dataframe(df.style.format({'Link': make_clickable}))
 
 # Line to run script in command prompt -> python -m streamlit run 
